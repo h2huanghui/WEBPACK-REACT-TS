c33e26455e270afaa9b5ce03ea5a219f360f140a
huanghui <815189517@qq.com>
2022-03-14 14:46:55 +0800

    modify index.tsx

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,95 +1,100 @@
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const ROOTPATH = path.join(process.cwd());
-const APP_PATH = path.join(ROOTPATH, '/src');
+const APP_PATH = path.join(ROOTPATH, "/src");
 
-const templateURI = (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')? '/template_test.html':'/template.html';
+const templateURI =
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
+    ? "/template_test.html"
+    : "/template.html";
 
 module.exports = {
   entry: {
-    'index': path.join(APP_PATH, 'entry.js'),
+    index: path.join(APP_PATH, "index.tsx"),
   },
   // entry: path.join(APP_PATH, 'entry.js'),
   output: {
-    publicPath: '/',
-    filename: 'static/js/[name].[chunkhash:8].js',
+    publicPath: "/",
+    filename: "static/js/[name].[chunkhash:8].js",
     // chunkFilename: 'js/[name].[chunkhash].js',
-    path: path.join(ROOTPATH, '/dist')
+    path: path.join(ROOTPATH, "/dist"),
   },
 
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: 'babel-loader',
-      exclude: /node_modules/
-    },
-    {
-      test: /\.tsx?$/,
-      use: ['babel-loader','ts-loader'],
-      exclude: /node_modules/
-    },
-    {
-      test: /\.css$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        'css-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            config: {
-              path: 'postcss.config.js'
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: {
+                path: "postcss.config.js",
+              },
             },
-          }
-        }
-      ]
-    },
-    {
-      test: /\.less$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        'css-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            config: {
-              path: 'postcss.config.js'
-            },
-          }
-        },
-        {
-          loader: 'less-loader',
-          options: {
-            modifyVars: {
-              'hack': `true; @import "${APP_PATH}/styles/theme.less";`,
-            },
-            javascriptEnabled: true,
           },
-        }
-      ]
-    },
-    {
-      test: /\.(eot|woff|ttf|woff2|svg|gif|png|jpg)(\?|$)/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[folder]/[name].[hash:8].[ext]',
-          outputPath: './static/assets'
-        }
-      }
-    }
-    ]
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: {
+                path: "postcss.config.js",
+              },
+            },
+          },
+          {
+            loader: "less-loader",
+            options: {
+              modifyVars: {
+                hack: `true; @import "${APP_PATH}/styles/theme.less";`,
+              },
+              javascriptEnabled: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|woff|ttf|woff2|svg|gif|png|jpg)(\?|$)/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[folder]/[name].[hash:8].[ext]",
+            outputPath: "./static/assets",
+          },
+        },
+      },
+    ],
   },
   resolve: {
+    extensions: [".tsx", ".ts", ".js"],
     alias: {
-      '@': `${APP_PATH}/`,
-      '@icon': `${APP_PATH}/components/Icon`,
-      '@ant-design/icons/lib/dist$': `${APP_PATH}/utils/antdIcon.js`
-    }
+      "@": `${APP_PATH}/`,
+      "@icon": `${APP_PATH}/components/Icon`,
+      "@ant-design/icons/lib/dist$": `${APP_PATH}/utils/antdIcon.js`,
+    },
   },
   optimization: {
     concatenateModules: false,
@@ -97,53 +102,53 @@ module.exports = {
       cacheGroups: {
         libs: {
           test: /(react|react-dom|react-dom-router|babel-polyfill|axios|qs|intl|core-js)/,
-          chunks: 'all',
-          name: 'libs',
+          chunks: "all",
+          name: "libs",
           priority: 10,
-          enforce: true
+          enforce: true,
         },
         ui: {
           test: /(antd|lottie-web)/,
-          chunks: 'all',
-          name: 'ui',
+          chunks: "all",
+          name: "ui",
           priority: 9,
-          enforce: true
+          enforce: true,
         },
         uiRc: {
           test: /(rc-.*)/,
-          chunks: 'all',
-          name: 'ui-rc',
+          chunks: "all",
+          name: "ui-rc",
           priority: 9,
-          enforce: true
+          enforce: true,
         },
         common: {
-          chunks: 'all',
+          chunks: "all",
           minChunks: 2,
-          name: 'common',
-          priority: 8, 
-          enforce: true
-        }
-      }
-    }
+          name: "common",
+          priority: 8,
+          enforce: true,
+        },
+      },
+    },
   },
   // performance: {
   //   hints: 'warning'
   // },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
+      filename: "index.html",
       template: path.join(APP_PATH, templateURI),
-      chunks: ['libs','index','ui','ui-rc','common'],
-      favicon: path.join(APP_PATH, 'assets/favicon/favicon.ico'),
+      chunks: ["libs", "index", "ui", "ui-rc", "common"],
+      favicon: path.join(APP_PATH, "assets/favicon/favicon.ico"),
     }),
     new MiniCssExtractPlugin({
-      filename: 'static/css/[name].[contenthash:8].css',
-      chunkFilename: 'static/css/[name].[contenthash:8].css',
-      allChunks: true
+      filename: "static/css/[name].[contenthash:8].css",
+      chunkFilename: "static/css/[name].[contenthash:8].css",
+      allChunks: true,
     }),
     new webpack.ContextReplacementPlugin(
       /moment[\/\\]locale/,
       /(en-gb|zh-cn)\.js/
-    )
-  ]
+    ),
+  ],
 };
---
+++ src/view/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App: React.FC = () => {
+  return <div className="App">TEST</div>;
+};
+
+export default App;
