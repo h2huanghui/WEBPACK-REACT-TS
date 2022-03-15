46a5c76d7e535243add06d25eeca3ab234f05d5f
huanghui <815189517@qq.com>
2022-03-15 17:32:06 +0800

    class

--- src/components/demo/Hello.tsx
+++ src/components/demo/Hello.tsx
@@ -3,8 +3,22 @@ import { Button } from 'antd';
 
 interface Greeting {
   name: string;
+  firstName: string;
+  lastName: string;
 }
 
-const Hello = (props: Greeting) => <Button type='primary'>{props.name}</Button>;
+const Hello = (props: Greeting) => <Button>{props.name}</Button>;
+
+// const Hello: React.FC<Greeting> = ({
+//   name,
+//   firstName,
+//   lastName,
+//   children
+// }) => <Button>{name}</Button>;
+
+Hello.defaultProps = {
+  firstName: '',
+  lastName: ''
+};
 
 export default Hello;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './components/demo/Hello';
+// import Hello from './components/demo/Hello';
+import HelloClass from './components/demo/HelloClass';
 import '@/styles/global.less';
 
-ReactDOM.render(<Hello name="TypeScript" />, document.getElementById('root'));
+ReactDOM.render(<HelloClass name="TypeScript" />, document.getElementById('root'));
