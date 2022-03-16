0a65252006472cc1733f7a62aea0326441dc2c86
huanghui <815189517@qq.com>
2022-03-16 10:36:04 +0800

    hooks

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Hello from './components/demo/Hello';
 // import HelloClass from './components/demo/HelloClass';
-import HelloHOC from './components/demo/HelloHOC';
+// import HelloHOC from './components/demo/HelloHOC';
+import HelloHooks from './components/demo/HelloHooks';
 import '@/styles/global.less';
 
-ReactDOM.render(<HelloHOC name="TypeScript" loading={false} />, document.getElementById('root'));
+ReactDOM.render(<HelloHooks name="TypeScript" />, document.getElementById('root'));
